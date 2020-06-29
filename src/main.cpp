156f210da4bef45f8f070e1457c7e7868a7ebe20
Pushkar Mehendale <mehenpm@gmail.com>
2020-06-28 20:31:09 -0500

    refactor: inlcuded header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
+#include "calibration.h"
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
+#include <iostream>
+#include <utility>
 
-int main() {
+int main(int argc, char **argv) {
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{640};
   constexpr std::size_t kScreenHeight{640};
-  constexpr std::size_t kGridWidth{32};
-  constexpr std::size_t kGridHeight{32};
+  constexpr std::size_t kGridWidth{width};
+  constexpr std::size_t kGridHeight{height};
 
   Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
   Controller controller;
