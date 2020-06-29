9ebb2936fedc4d8276e4cda7b29c982809cc290d
Pushkar Mehendale <mehenpm@gmail.com>
2020-06-28 20:10:09 -0500

    refactor
    - Added computer_snake
    - Included variable by reference

--- src/renderer.h
+++ src/renderer.h
@@ -1,20 +1,21 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
-#include <vector>
-#include "SDL.h"
 #include "snake.h"
+#include <SDL2/SDL.h>
+#include <vector>
 
 class Renderer {
- public:
-  Renderer(const std::size_t screen_width, const std::size_t screen_height,
-           const std::size_t grid_width, const std::size_t grid_height);
+public:
+  Renderer(const std::size_t &screen_width, const std::size_t &screen_height,
+           const std::size_t &grid_width, const std::size_t &grid_height);
   ~Renderer();
 
-  void Render(Snake const snake, SDL_Point const &food);
-  void UpdateWindowTitle(int score, int fps);
+  void Render(Snake const &snake, SDL_Point const &food,
+              Snake const &computer_snake);
+  void UpdateWindowTitle(int &&score_1, int &&score_2, int &fps);
 
- private:
+private:
   SDL_Window *sdl_window;
   SDL_Renderer *sdl_renderer;
 
