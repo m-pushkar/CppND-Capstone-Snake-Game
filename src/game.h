8b9da6f885d6eb9ee035511c9122d51b7ad485c6
Pushkar Mehendale <mehenpm@gmail.com>
2020-06-28 20:36:31 -0500

    refactor: updated game.h

--- src/game.h
+++ src/game.h
@@ -1,30 +1,36 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <random>
-#include "SDL.h"
+#include "computer_snake.h"
 #include "controller.h"
 #include "renderer.h"
 #include "snake.h"
+#include <SDL2/SDL.h>
+#include <chrono>
+#include <future>
+#include <mutex>
+#include <random>
+#include <thread>
+#include <vector>
 
 class Game {
- public:
-  Game(std::size_t grid_width, std::size_t grid_height);
+public:
+  Game(std::size_t &grid_width, std::size_t &grid_height);
   void Run(Controller const &controller, Renderer &renderer,
-           std::size_t target_frame_duration);
+           std::size_t &target_frame_duration);
   int GetScore() const;
   int GetSize() const;
 
- private:
+private:
   Snake snake;
+  Computer_snake computer_snake;
   SDL_Point food;
+  std::mutex game_mutex;
 
   std::random_device dev;
   std::mt19937 engine;
   std::uniform_int_distribution<int> random_w;
   std::uniform_int_distribution<int> random_h;
-
-  int score{0};
 
   void PlaceFood();
   void Update();
